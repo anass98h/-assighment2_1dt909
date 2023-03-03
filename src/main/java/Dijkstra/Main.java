721025002a5782c6e268e3334568fc40895d930e
Hallak Mohamadanas <hm222ua@student.lnu.se>
2023-03-03 23:07:15 +0100

    Dijkstra v0.5

---
+++ src/main/java/Dijkstra/Main.java
@@ -0,0 +1,33 @@
+package Dijkstra;
+
+import Dijkstra.DiEdge;
+import Dijkstra.DijkstraSP;
+import Dijkstra.EWDiGraph;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EWDiGraph ewd = new EWDiGraph(8);
+        ewd.addEdge(new DiEdge(0, 5, 0.34));
+        ewd.addEdge(new DiEdge(5, 3, 0.34));
+        ewd.addEdge(new DiEdge(0, 2, 0.64));
+        ewd.addEdge(new DiEdge(1, 4, 0.34));
+        ewd.addEdge(new DiEdge(4, 1, 0.64));
+        ewd.addEdge(new DiEdge(5, 6, 0.34));
+        ewd.addEdge(new DiEdge(6, 0, 0.64));
+        ewd.addEdge(new DiEdge(6, 4, 0.34));
+        ewd.addEdge(new DiEdge(0, 1, 0.34));
+        int s = 0;
+        DijkstraSP sp = new DijkstraSP(ewd, s);
+
+        for(var v=1; v< ewd.V(); v++){
+            if(sp.distTo(v)< Double.POSITIVE_INFINITY){
+                System.out.printf("%d to %d (%f): ",s,v,sp.distTo(v));
+
+
+            } else {
+                System.out.printf("%d to %d: no path\n ", s, v);
+            }
+        }
+    }
+}
