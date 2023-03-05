1cd97d1889d4e7d7d73e251217768fb6a50dfbe6
Hallak Mohamadanas <hm222ua@student.lnu.se>
2023-03-05 22:45:52 +0100

    NS parralale

--- src/main/java/NarySearchp/Main.java
+++ src/main/java/NarySearchp/Main.java
@@ -1,28 +1,24 @@
 package NarySearchp;
 
 
-import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25};
-        int key = 9;
-        int numThreads = 2;
-        int parallelValues = 3;
+    public static void main(String[] args) {
+        try {
+            int[] arr = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+            int key = 8;
+            int lo = 0;
+            int hi = arr.length - 1;
+            int intv = 4;
 
-        int index = NarySearch.nary(arr, 0, arr.length - 1, key, numThreads, parallelValues);
-        System.out.println("Index of " + key + " is " + index);
-
-        // Test with different number of threads and parallel values
-        int[] numThreadsArr = {1, 2, 4};
-        int[] parallelValuesArr = {1, 2, 3};
-        for (int i = 0; i < numThreadsArr.length; i++) {
-            for (int j = 0; j < parallelValuesArr.length; j++) {
-                int nt = numThreadsArr[i];
-                int pv = parallelValuesArr[j];
-                int idx = NarySearch.nary(arr, 0, arr.length - 1, key, nt, pv);
-                System.out.println("With " + nt + " threads and " + pv + " parallel values: " + idx);
+            int result = NarySearch.nary(arr, lo, hi, key, intv);
+            if (result == -1) {
+                System.out.println("Key not found in the array.");
+            } else {
+                System.out.println("Key found at index " + result + " in the array.");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
--- src/main/java/NarySearchp/NarySearch.java
+++ src/main/java/NarySearchp/NarySearch.java
@@ -1,92 +1,52 @@
 package NarySearchp;
 
-import java.util.concurrent.*;
+
+import static java.lang.Math.floor;
 
 public class NarySearch {
 
-    public static int nary(int[] A, int lo, int hi, int key, int intv, int numThreads) throws InterruptedException, ExecutionException {
+    public static int nary(int[] A, int lo, int hi, int key, int intv) {
         int[] mid = new int[intv + 1];
         char[] locate = new char[intv + 2];
-        int pos = Integer.MIN_VALUE;
+        int pos = -1;
         locate[0] = 'R';
         locate[intv + 1] = 'L';
-
-        int chunkSize = (hi - lo + 1) / numThreads;
-
-        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-        Future<Integer>[] futures = new Future[numThreads];
-
-        for (int t = 0; t < numThreads; t++) {
-            int startIndex = lo + t * chunkSize;
-            int endIndex = (t == numThreads - 1) ? hi + 1 : startIndex + chunkSize;
-            futures[t] = executor.submit(new MarkLocTask(A, mid, locate, startIndex, endIndex, intv, key));
-        }
-
-        for (int t = 0; t < numThreads; t++) {
-            pos = futures[t].get();
-            if (pos != Integer.MIN_VALUE) {
-                break;
+        while (lo <= hi && pos == Integer.MIN_VALUE) {
+            mid[0] = lo -1;
+            int step = (int) Math.floor( (hi - lo + 1)/(intv + 1));
+            pos = markLoc(A, mid, locate, lo, hi, step, intv, key,pos);
+            for (int i = 1; i <= intv; i++) {
+                if (locate[i] != locate[i - 1]) {
+                    lo = mid[i - 1] + 1;
+                    hi = mid[i] - 1;
+                }
+            }
+            if (locate[intv] != locate[intv + 1]) {
+                lo = mid[intv] + 1;
             }
         }
-
-        executor.shutdown();
-
-        for (int i = 1; i <= intv; i++) {
-            if (locate[i] != locate[i - 1]) {
-                lo = mid[i - 1] + 1;
-                hi = mid[i] - 1;
-            }
-        }
-        if (locate[intv] != locate[intv + 1]) {
-            lo = mid[intv] + 1;
-        }
-
         return pos;
     }
 
-    private static class MarkLocTask implements Callable<Integer> {
-        private final int[] A;
-        private final int[] mid;
-        private final char[] locate;
-        private final int startIndex;
-        private final int endIndex;
-        private final int intv;
-        private final int key;
-
-        public MarkLocTask(int[] A, int[] mid, char[] locate, int startIndex, int endIndex, int intv, int key) {
-            this.A = A;
-            this.mid = mid;
-            this.locate = locate;
-            this.startIndex = startIndex;
-            this.endIndex = endIndex;
-            this.intv = intv;
-            this.key = key;
-        }
-
-        public Integer call() {
-            int pos = Integer.MIN_VALUE;
-            int step = (endIndex - startIndex) / (intv + 1);
-
-            for (int i = 1; i <= intv; i++) {
-                int offs = step * i + (i - 1);
-                mid[i] = startIndex + offs;
-                int lmid = startIndex + offs;
-                if (lmid < endIndex) {
-                    if (A[lmid] > key) {
-                        locate[i] = 'L';
-                    } else if (A[lmid] < key) {
-                        locate[i] = 'R';
-                    } else {
-                        locate[i] = 'E';
-                        pos = lmid;
-                    }
-                } else {
-                    mid[i] = endIndex;
+    private static int markLoc(int[] A, int[] mid, char[] locate, int lo, int hi, int step, int intv, int key,int pos) {
+        for (int i = 1; i <= intv; i++) {
+            int offs = step * i + (i - 1);
+            mid[i] = lo + offs;
+            int lmid = lo + offs;
+            if (lmid <= hi) {
+                if (A[lmid] > key) {
                     locate[i] = 'L';
+                } else if (A[lmid] < key) {
+                    locate[i] = 'R';
+                } else {
+                    locate[i] = 'E';
+                    pos = lmid;
                 }
+            } else {
+                mid[i] = hi + 1;
+                locate[i] = 'L';
             }
-
-            return pos;
         }
+        return pos;
     }
 }
