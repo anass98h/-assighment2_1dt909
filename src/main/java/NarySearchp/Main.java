6927917c5decbe9d0b2275712c3e236097190f3d
Hallak Mohamadanas <hm222ua@student.lnu.se>
2023-03-06 00:49:34 +0100

    works 100%

--- src/main/java/NarySearchp/Main.java
+++ src/main/java/NarySearchp/Main.java
@@ -1,17 +1,48 @@
 package NarySearchp;
 
 
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] A = {1, 2, 3, 4, 5, 8, 10, 21, 26, 29, 35, 38, 39};
-        int lo = 0;
-        int hi = A.length - 1;
-        int key = 4;
-        int intv = 2;
-        int numThreads = 4;
+        runExperiment(10000000,10100000,2,6);
+    }
+    public static void runExperiment(int minimumListZise, int MaximumListSize, int MinmumNumberofThreads, int MaximumNumberOfThreads) {
+        Random rand = new Random();
 
-        int pos = NarySearch.nary(A, lo, hi, key, intv,numThreads);
-        System.out.println("Position of key: " + pos);
+        for (int listSize = minimumListZise; listSize <= MaximumListSize; listSize += 1000000) {
+
+
+            int[] A = new int[listSize];
+            for (int i = 0; i < listSize; i++) {
+                A[i] = rand.nextInt(listSize*2);
+            }
+
+
+
+
+
+                for (int numThreads = MinmumNumberofThreads; numThreads <= MaximumNumberOfThreads; numThreads++) {
+
+
+
+                    long BSstartTime = System.currentTimeMillis();
+
+                    BinarySearch bs = new BinarySearch();
+
+
+                    int result = bs.binarySearch(A,0,A.length-1,A[rand.nextInt(A.length)]);
+
+                    long BSendTime = System.currentTimeMillis();
+                    long startTime = System.currentTimeMillis();
+                    int pos = NarySearch.nary(A, 0, A.length-1, A[rand.nextInt(A.length)], 1, numThreads);
+                    long endTime = System.currentTimeMillis();
+
+
+                    System.out.println("List size: " + listSize  + " | Threads: " + numThreads + " | Time (ms): " + (endTime - startTime));
+                    System.out.println("List size: " + listSize  + " | Time (ms): " + (BSendTime - BSstartTime));
+                }
+
+        }
     }
 }
